89ba7486be58139d2319451ed4f3748250ae8401
givemin5 <givemin5@gmail.com>
2018-12-12 01:44:39 +0800

    Add Unit Test

--- LeetCodeDay1_TwoSum/UnitTest1.cs
+++ LeetCodeDay1_TwoSum/UnitTest1.cs
@@ -1,14 +1,28 @@
-﻿using System;
+﻿using ExpectedObjects;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace LeetCodeDay1_TwoSum
 {
+    //Given nums = [2, 7, 11, 15], target = 9,
+    //Because nums[0] + nums[1] = 2 + 7 = 9,
+    //return [0, 1].
+
     [TestClass]
-    public class UnitTest1
+    public class TwoSumTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TwoSum_Target_is_9_Then_Return_0_1()
         {
+            //Arrange
+            var nums = new[] { 2, 7, 11, 15 };
+
+            //Act
+            Solution solution = new Solution();
+            var actual = solution.TwoSum(nums, 9);
+
+            //Assert
+            var exptected = new[] { 0, 1 };
+            actual.ToExpectedObject().ShouldEqual(exptected);
         }
     }
 }
