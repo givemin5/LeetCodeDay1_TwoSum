51353df748c8e5b3e5de64216f95863d0f366dc6
givemin5 <givemin5@gmail.com>
2018-12-11 22:28:50 +0800

    Add project files.

---
+++ LeetCodeDay1_TwoSum/SourceTemplates.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+
+namespace LeetCodeDay1_TwoSum
+{
+    public static class SourceTemplates
+    {
+        [SourceTemplate]
+        [Macro(Target = "expected", Editable = 1)]
+        public static void ae<T>(this T source)
+        {
+            /*$ Assert.AreEqual( $expected$ , source);*/
+        }
+
+        [SourceTemplate]
+        public static void abs<T>(this T source)
+        {
+            /*$ Math.Abs(source)*/
+        }
+    }
+}
